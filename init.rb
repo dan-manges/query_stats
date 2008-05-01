79645310cf4e937cd24f08edbb5c7a19bc31ee13
daniel.manges <daniel.manges@71187760-d61d-0410-86ff-8714271a0d44>
2008-05-01 15:01:00 +0000

    prevent stack level too deep from multiple loads of init.rb
    git-svn-id: https://query-stats.googlecode.com/svn/trunk/query_stats@17 71187760-d61d-0410-86ff-8714271a0d44

--- init.rb
+++ init.rb
@@ -2,7 +2,9 @@ require 'benchmark'
 
 if defined?(Rails::VERSION::STRING) && Rails::VERSION::STRING < "1.2.0"
   $stderr.puts "The QueryStats plugin requires Rails >= 1.2.0"
-else
+elsif defined?(Rails::VERSION::STRING) && Rails::VERSION::STRING >= "2.0.0"
+  $stderr.puts "The QueryStats plugin does not work with Rails 2.0 (yet)"
+elsif !ActionController::Base.included_modules.include?(QueryStats::Labeler)
   ActiveRecord::Base.connection.class.send :include, QueryStats::Recorder
   ActionController::Base.send :include, QueryStats::Labeler
   ActionController::Benchmarking.send :include, QueryStats::Logger
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,9 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 RAILS_ROOT = File.dirname(__FILE__)
 
-require 'rubygems'
 require 'test/unit'
+require 'rubygems'
+gem "rails", "1.2.6"
 require 'active_record'
 require 'active_record/fixtures'
 require 'active_support/binding_of_caller'
@@ -20,7 +21,11 @@ require 'query_stats/holder'
 require 'query_stats/labeler'
 require 'query_stats/logger'
 require 'query_stats/recorder'
+
 require "#{File.dirname(__FILE__)}/../init"
+# make sure multiple requires don't create stack level too deep error
+load "#{File.dirname(__FILE__)}/../init.rb"
+
 load(File.dirname(__FILE__) + "/schema.rb") if File.exist?(File.dirname(__FILE__) + "/schema.rb")
 
 Test::Unit::TestCase.fixture_path = File.dirname(__FILE__) + "/fixtures/"
